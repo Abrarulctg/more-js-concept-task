109cbd5e4ef9345d1ebc102db603890b9f9aa7ac
Abrarulctg <jubairctg99@gmail.com>
2024-03-02 12:35:29 +0600

    started module-35: more js concept task

---
+++ task-1.js
@@ -0,0 +1,14 @@
+// 1a) Write a function that displays a message after 5s.
+
+
+function displayMessageAfter5s() {
+
+    setTimeout(function () {
+        console.log("Displaying message after 5s")
+    }, 5000);
+
+    console.log("Hellow.............. Normal console");
+}
+
+
+displayMessageAfter5s()
